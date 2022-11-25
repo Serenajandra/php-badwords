a09964871c58fde26f36d60fbe495e37141e6923
Serenajandra <serena.jandra@gmail.com>
2022-11-25 10:09:39 +0100

    aggiunto strlen

--- esegui.php
+++ esegui.php
@@ -3,7 +3,8 @@
     $censura = $_GET["tua_censura"]; 
     $stringa_censurata = str_replace($censura, "***", $tuo_testo); 
 ?>
-
+<!-- <?php var_dump($censura);
+?> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,10 +18,14 @@
 <body>
     <h5>Ecco il tuo testo senza filtri né veli ;) </h5>
     <p><?php echo $tuo_testo; ?></p>
+    <p>Numero di caratteri di cui è composto:</p>
+    <?php echo strlen($tuo_testo) ?>
+    
     <h4>Questa invece è la versione a cui hai deciso di applicare la censura:</h4>
 <p>
     <?php echo $stringa_censurata; ?>
 </p>
+<span>La stringa censurata è composta da </span> <?php echo strlen($stringa_censurata) ?> <span>caratteri</span>
 <hr>
 <a href="index.php">Cancella</a>
 </body>
